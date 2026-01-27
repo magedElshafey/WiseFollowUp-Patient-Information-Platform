b6b378ad0e3a0c448b2c57fe6a3675d6f91766a9
magedElshafey <magedelshafey98@gmail.com>
2026-01-27 12:33:43 +0200

    updates

--- src/features/home/components/hero/text-block/HeroTextBlock.tsx
+++ src/features/home/components/hero/text-block/HeroTextBlock.tsx
@@ -1,4 +1,5 @@
-import { parseHeroTitle } from "../utils/parseHeroTitle";
+import HtmlConverter from "@/common/components/htmlConverter/HtmlConverter";
+// import { parseHeroTitle } from "../utils/parseHeroTitle";
 
 type Props = {
   title?: string;
@@ -6,18 +7,19 @@ type Props = {
 };
 
 const HeroTextBlock: React.FC<Props> = ({ title, desc }) => {
-  const parsedTitle = parseHeroTitle(title);
+  // const parsedTitle = parseHeroTitle(title);
 
   return (
     <div className="text-center">
       <h1
         id="home-hero-heading"
-        className="min-h-[3rem] text-2xl md:text-4xl font-bold tracking-tight text-text-primary"
+        className="!min-h-[2rem] !text-2xl !md:text-4xl !font-bold !tracking-tight text-text-primary"
       >
-        {parsedTitle.highlight && (
+        <HtmlConverter html={title} />
+        {/* {parsedTitle.highlight && (
           <span className="text-primary mr-2">{parsedTitle.highlight}</span>
         )}
-        <span>{parsedTitle.rest}</span>
+        <span>{parsedTitle.rest}</span> */}
       </h1>
 
       <p className="mt-4 min-h-[2.5rem] text-sm md:text-base text-text-muted max-w-2xl mx-auto">
