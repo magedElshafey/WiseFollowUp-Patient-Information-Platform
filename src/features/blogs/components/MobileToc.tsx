dd5b82a093429d76e59503d6d178909816d2b80d
magedElshafey <magedelshafey98@gmail.com>
2026-01-27 15:51:00 +0200

    updates

--- src/features/blogs/components/MobileToc.tsx
+++ src/features/blogs/components/MobileToc.tsx
@@ -29,7 +29,7 @@ const MobileToc: React.FC<Props> = ({ tocItems, activeId }) => {
         type="button"
         onClick={() => setOpen(true)}
         className="
-          lg:hidden fixed bottom-4 right-4 z-50
+          lg:hidden fixed bottom-4 right-4 z-30
           rounded-pill
           bg-primary text-white
           px-4 py-2 text-xs font-semibold
